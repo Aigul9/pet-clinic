efdeb5e52d3668aeff4f85034752dc803c9ecc6b
Aigul <aigul.sibgatulli@mail.ru>
2021-11-27 16:25:03 +0300

    create base entity, closes #37

--- pet-clinic-data/src/main/java/springframework/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/springframework/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package springframework.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// base class for jpa, we'll inherit from this
+// won't create another table for this entity
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
