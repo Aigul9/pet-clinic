e47b46f1237f3545db0df147cd89e86fbe9cdb7a
Aigul <aigul.sibgatulli@mail.ru>
2021-11-28 23:36:55 +0300

    impl jpa service for vets, closes #43

---
+++ pet-clinic-data/src/main/java/springframework/services/jpa/VetJpaService.java
@@ -0,0 +1,51 @@
+package springframework.services.jpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import springframework.model.Vet;
+import springframework.repositories.VetRepository;
+import springframework.services.VetService;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        Optional<Vet> optionalVet = vetRepository.findById(id);
+        return optionalVet.orElseThrow(() -> new NoSuchElementException("Vet is not found, id: " + id));
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
