dada6bdacdfbe6b6faff62248709847bf991acba
Aigul <aigul.sibgatulli@mail.ru>
2021-11-28 23:30:37 +0300

    impl jpa service for owners, closes #42

---
+++ pet-clinic-data/src/main/java/springframework/services/jpa/OwnerJpaService.java
@@ -0,0 +1,56 @@
+package springframework.services.jpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import springframework.model.Owner;
+import springframework.repositories.OwnerRepository;
+import springframework.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        Optional<Owner> optionalOwner = ownerRepository.findById(id);
+        return optionalOwner.orElseThrow(() -> new NoSuchElementException("Owner is not found, id: " + id));
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
